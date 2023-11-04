a2401dcac2bbcb11e7f8c2733f36546775341da6
nanda6523 <147233831+nanda6523@users.noreply.github.com>
2023-11-04 10:27:21 +0700

    Add files via upload

---
+++ Perulangan100nb.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author anand
+ */
+public class Perulangan100nb {
+    public static void main(String[] args) {
+        String nama = "Ammar Siraj Ananda";
+        for(int a = 1 ; a<=100; a++){
+            if(a<10){
+                System.out.println(a);
+            }
+            else{
+                System.out.println(nama);
+            }
+        }
+    }
+}
